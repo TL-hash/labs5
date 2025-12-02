591d8df8e72ba6a8112912a03a1e993728f523f4
TL-hash <terem158@mail.ru>
2025-12-02 21:11:16 +0300

    Adding task 1 (ind)

---
+++ task1_ind.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# ~*~ coding: utf-8 ~*~
+
+if __name__ == "__main__":
+    u = set("abcdefghijklmnopqrstuvwxyz")
+
+    a = {"b", "f", "g", "m", "o"}
+    b = {"b", "g", "h", "l", "u"}
+    c = {"e", "f", "m"}
+    d = {"e", "g", "l", "p", "q", "u", "v"}
+
+    x = (a.intersection(c).union(b))
+    print(f"x = {x}")
+
+    bn = u.difference(b)
+
+    y = (a.intersection(bn).union(c.difference(d)))
+    print(f"y = {y}")
